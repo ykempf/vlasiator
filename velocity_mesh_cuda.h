09ace9c5bf0af3e90c02349aae2466131acbbe8a
Sebastian von Alfthan <sebastian.von.alfthan@fmi.fi>
2015-04-27 12:45:00 +0300

    Init offsets, streams

--- velocity_mesh_cuda.h
+++ velocity_mesh_cuda.h
@@ -30,8 +30,7 @@ namespace vmesh {
    public:
       __host__ void init(Realf *h_data, GID *h_blockIDs, uint nBlocks);
       __host__ void clear();
-
-   private:
+      __device__ __host__ uint size();
       Realf dv;      
       uint nBlocks;
       Realf *data;
@@ -39,9 +38,12 @@ namespace vmesh {
       LID *blockOffset;
    };
 
-   template<typename GID, typename LID> __host__ VelocityMeshCuda<GID,LID>* createVelocityMeshCuda(Realf *h_data, GID *h_blockIDs, uint nBlocks);
-   template<typename GID, typename LID> __host__ void destroyVelocityMeshCuda(VelocityMeshCuda<GID, LID> *d_vmesh);
+   template<typename GID, typename LID> __host__ VelocityMeshCuda<GID,LID>* createVelocityMeshCuda(Realf *h_data, GID *h_blockIDs, LID nBlocks, cudaStream_t stream);
+   template<typename GID, typename LID> __host__ void destroyVelocityMeshCuda(VelocityMeshCuda<GID, LID> *d_vmesh, cudaStream_t stream);
    template<typename GID, typename LID> __global__ void sortVelocityBlocks(VelocityMeshCuda<GID, LID> *d_vmesh, uint dimension);
+   template<typename GID, typename LID> __global__ void initBlockOffsets(VelocityMeshCuda<GID,LID> *d_vmesh);
+   template<typename GID, typename LID> __global__ void testBlockOffsets(VelocityMeshCuda<GID,LID> *d_vmesh, int testId);
+
    
 /*----------------------------------------CLASS functions ------------------------------------------*/
    
@@ -54,6 +56,7 @@ namespace vmesh {
       nBlocks=h_nBlocks;
       cudaMalloc(&data, nBlocks * WID3 * sizeof(Realf));
       cudaMalloc(&blockIDs, nBlocks * sizeof(GID));
+      cudaMalloc(&blockOffset, nBlocks * sizeof(LID));
       
       cudaEventRecord(evA);      
       cudaMemcpy(data, h_data, nBlocks *  WID3 * sizeof(Realf), cudaMemcpyHostToDevice);
@@ -71,28 +74,53 @@ namespace vmesh {
    template<typename GID, typename LID> __host__ void VelocityMeshCuda<GID,LID>::clear(){
       cudaFree(data);
       cudaFree(blockIDs);
+      cudaFree(blockOffset);
    }
-
+   
+   template<typename GID, typename LID> __device__ __host__ uint VelocityMeshCuda<GID,LID>::size(){
+      return nBlocks;
+   }
+   
 /*---------------------------------------- INTERFACE functions ------------------------------------------*/
+
+   
    
    template<typename GID,typename LID>
-   __host__ VelocityMeshCuda<GID,LID>* createVelocityMeshCuda(Realf *h_data, GID *h_blockIDs, LID nBlocks) {
+   __host__ VelocityMeshCuda<GID,LID>* createVelocityMeshCuda(Realf *h_data, GID *h_blockIDs, LID nBlocks, cudaStream_t stream) {
       VelocityMeshCuda<GID,LID> h_vmesh;
       VelocityMeshCuda<GID,LID> *d_vmesh;
       //allocate space on device for device resident class
-      cudaMalloc((void **)&d_vmesh, sizeof(VelocityMeshCuda<GID,LID>));      
+      cudaMalloc((void **)&d_vmesh, sizeof(VelocityMeshCuda<GID, LID>));      
       //init (private) members on host
       h_vmesh.init(h_data, h_blockIDs, nBlocks);
       //copy all  members to device
-      cudaMemcpy(d_vmesh, &h_vmesh, sizeof(VelocityMeshCuda<GID,LID>), cudaMemcpyHostToDevice);
+      cudaMemcpyAsync(d_vmesh, &h_vmesh, sizeof(VelocityMeshCuda<GID, LID>), cudaMemcpyHostToDevice, stream);
       return d_vmesh;
-      //h_vmesh will now be deallocated
+       //h_vmesh will now be deallocated
+   }
+
+   template<typename GID, typename LID> __global__ void initBlockOffsets(VelocityMeshCuda<GID,LID> *d_vmesh){
+      int id = blockIdx.x*blockDim.x+threadIdx.x;
+      if (id < d_vmesh->nBlocks ){
+         d_vmesh->blockOffset[id] = id;
+      }
+   }
+   template<typename GID, typename LID> __global__ void testBlockOffsets(VelocityMeshCuda<GID,LID> *d_vmesh, int testId) {
+      int id = blockIdx.x*blockDim.x+threadIdx.x;
+      if( testId == id && id < d_vmesh->nBlocks ){
+         printf("id %d (block %d, thread %d) has a value of %d\n",id, blockIdx.x, threadIdx.x, d_vmesh->blockOffset[id]);
+      }
+   }
+   template<typename GID, typename LID> __global__ void sortVelocityBlocks(VelocityMeshCuda<GID, LID> *d_vmesh, uint dimension){
+
+
    }
    
-   template<typename GID,typename LID> __host__ void destroyVelocityMeshCuda(VelocityMeshCuda<GID,LID> *d_vmesh) {
+   template<typename GID, typename LID> __host__ void destroyVelocityMeshCuda(VelocityMeshCuda<GID,LID> *d_vmesh, cudaStream_t stream) {
       VelocityMeshCuda<GID,LID> h_vmesh;
-      //copy all  members to host (not deep)
-      cudaMemcpy(&h_vmesh, d_vmesh, sizeof(VelocityMeshCuda<GID,LID>), cudaMemcpyDeviceToHost);
+      //copy all  members to host (not deep), not async since we need to 
+      cudaMemcpyAsync(&h_vmesh, d_vmesh, sizeof(VelocityMeshCuda<GID,LID>), cudaMemcpyDeviceToHost, stream);
+      cudaStreamSynchronize(stream);
       //clear data in velocity mesh
       h_vmesh.clear();
       //free also the mesh itself
--- vlasovsolver/cuda_acc_map.cu
+++ vlasovsolver/cuda_acc_map.cu
@@ -2,14 +2,28 @@
 
 
 bool accelerateVelocityMeshCuda(Realf **blockDatas, vmesh::GlobalID **blockIDs, vmesh::LocalID *nBlocks, const uint nCells ){
-//   cudaStream_t streams[nCells];   
+   cudaStream_t streams[nCells];
    for (int i = 0; i < nCells; i++) {
-     // TODO, put each cell into a separate stream:      cudaStreamCreate(&streams[i]);
-      vmesh::VelocityMeshCuda<vmesh::GlobalID, vmesh::LocalID> *d_vmesh =
-         vmesh::createVelocityMeshCuda(blockDatas[i], blockIDs[i], nBlocks[i]);
-
-      vmesh::destroyVelocityMeshCuda(d_vmesh);
+      cudaStreamCreate(&streams[i]);
    }
    
-   cudaDeviceSynchronize();
+   for (int i = 0; i < nCells; i++) {
+      vmesh::VelocityMeshCuda<vmesh::GlobalID, vmesh::LocalID> *d_vmesh =
+         vmesh::createVelocityMeshCuda(blockDatas[i], blockIDs[i], nBlocks[i], streams[i]);
+      int blockSize = 512; 
+      int gridSize = 1 + nBlocks[i] / blockSize; // value determine by block size and total work
+      cudaStreamSynchronize(streams[i]);
+      vmesh::initBlockOffsets<<<gridSize, blockSize, 0, streams[i]>>>(d_vmesh);
+      cudaStreamSynchronize(streams[i]);
+      vmesh::testBlockOffsets<<<gridSize, blockSize, 0, streams[i]>>>(d_vmesh, 610);
+      
+      vmesh::destroyVelocityMeshCuda(d_vmesh, streams[i]);
+   }
+
+
+   for (int i = 0; i < nCells; i++) {
+      cudaStreamDestroy(streams[i]);
+   }
+
+
 }
