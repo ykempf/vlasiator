1909efc94302113f9862209b441b13533ddac51a
Yann Pfau-Kempf <yann.kempf@helsinki.fi>
2023-11-02 13:50:24 +0200

    Allow multiple boxes for refinement tests.

--- grid.cpp
+++ grid.cpp
@@ -215,7 +215,7 @@ void initializeGrids(
 
       if (P::forceRefinement) {
          phiprof::Timer timer {"Restart refinement"};
-         for (uint i = 0; i < P::amrMaxSpatialRefLevel; ++i) {
+         for (int i = 0; i < P::amrMaxSpatialRefLevel; ++i) {
             if (!adaptRefinement(mpiGrid, technicalGrid, sysBoundaries, project, i)) {
                cerr << "(MAIN) ERROR: Forcing refinement takes too much memory" << endl;
                exit(1);
@@ -224,7 +224,7 @@ void initializeGrids(
          }
       } else if (P::refineOnRestart) {
          phiprof::Timer timer {"Restart refinement"};
-         for (uint i = 0; i < P::amrMaxSpatialRefLevel; ++i) {
+         for (int i = 0; i < P::amrMaxSpatialRefLevel; ++i) {
             adaptRefinement(mpiGrid, technicalGrid, sysBoundaries, project);
             balanceLoad(mpiGrid, sysBoundaries);
          }
